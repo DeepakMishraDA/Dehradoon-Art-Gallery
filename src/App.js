a191253d78c713378e61dd6944aa2d5f90bf225e
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-17 00:22:38 +0100

    desiging the page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { ButtonGroup } from "@material-ui/core";
+import Button from "@material-ui/core/Button";
+import AccountBalanceTwoToneIcon from "@material-ui/icons/AccountBalanceTwoTone";
+import VpnKeyTwoToneIcon from "@material-ui/icons//VpnKeyTwoTone";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <ButtonGroup
+        variant="text"
+        color="primary"
+        aria-label="text primary button group"
+      >
+        <Button startIcon={<AccountBalanceTwoToneIcon />}>Gallery</Button>
+        <Button
+          endIcon={<VpnKeyTwoToneIcon />}
+          onClick={() =>
+            alert(
+              "Sorry you are not a member yet! Please register first. Thank You"
+            )
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Login
+        </Button>
+      </ButtonGroup>
     </div>
   );
 }
