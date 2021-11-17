cf590030f84296efbfc70735ca493d50fdb77b23
DeepakMishraDA <deepakvinodmishra@gmail.com>
2021-11-17 02:23:25 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,59 @@
+import * as React from "react";
 import { ButtonGroup } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
 import AccountBalanceTwoToneIcon from "@material-ui/icons/AccountBalanceTwoTone";
 import VpnKeyTwoToneIcon from "@material-ui/icons//VpnKeyTwoTone";
+import Box from "@material-ui/core/Box";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 function App() {
+  const [value, setValue] = React.useState(0);
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
     <div className="App">
-      <ButtonGroup
-        variant="text"
-        color="primary"
-        aria-label="text primary button group"
+      <Box
+        m={0.5}
+        pl={125}
+        style={{ borderBottom: ".5px solid", borderColor: "pink" }}
       >
-        <Button startIcon={<AccountBalanceTwoToneIcon />}>Gallery</Button>
-        <Button
-          endIcon={<VpnKeyTwoToneIcon />}
-          onClick={() =>
-            alert(
-              "Sorry you are not a member yet! Please register first. Thank You"
-            )
-          }
+        <ButtonGroup
+          variant="text"
+          color="primary"
+          aria-label="text primary button group"
         >
-          Login
-        </Button>
-      </ButtonGroup>
+          <Button startIcon={<AccountBalanceTwoToneIcon />}>Gallery</Button>
+          <Button
+            endIcon={<VpnKeyTwoToneIcon />}
+            onClick={() =>
+              alert(
+                "Sorry you are not a member yet! Please register first. Thank You"
+              )
+            }
+          >
+            Login
+          </Button>
+        </ButtonGroup>
+      </Box>
+      <Box pl={25} sx={{ display: "flex" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          variant="scrollable"
+          scrollButtons={true}
+          aria-label="scrollable prevent tabs example"
+        >
+          <Tab label="Tanvi Singh"></Tab>
+          <Tab label="Bio" />
+          <Tab label="Photographs" />
+          <Tab label="Murals" />
+          <Tab label="Gigapixel Images" />
+          <Tab label="ShortFilms" />
+          <Tab label="Latest" />
+        </Tabs>
+      </Box>
     </div>
   );
 }
